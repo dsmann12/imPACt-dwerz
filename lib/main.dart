95c5ff27434bd521745e2483da1a69defb42b46d
leafBlader <zye3@lsu.edu>
2019-11-06 22:20:21 -0600

    Added template profile code for profile tab

--- lib/main.dart
+++ lib/main.dart
@@ -42,17 +42,17 @@ Widget displayHomeContent()
   );
 }
 
-Widget buildCoverImage(Size screenSize)
-{
-  return Container(
-    height: screenSize.height / 2.6,
-    decoration: BoxDecoration(
-      image: DecorationImage(image: AssetImage('assets/rubber_duck.jpg'),
-      fit: BoxFit.cover,
-      ),
-    ),
-  );
-}
+//Widget buildCoverImage(Size screenSize)
+//{
+//  return Container(
+//    height: screenSize.height / 2.6,
+//    decoration: BoxDecoration(
+//      image: DecorationImage(image: AssetImage('assets/rubber_duck.jpg'),
+//      fit: BoxFit.cover,
+//      ),
+//    ),
+//  );
+//}
 
 
 Widget displayMentorList() {
@@ -74,6 +74,120 @@ Widget displayMentorList() {
   );
 }
 
+class PersonalProfile extends StatefulWidget {
+  @override
+  _PersonalProfileState createState() => new _PersonalProfileState();
+}
+
+class _PersonalProfileState extends State<PersonalProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        body: new Stack(
+          children: <Widget>[
+            ClipPath(
+              child: Container(color: Colors.black.withOpacity(0.8)),
+              clipper: getClipper(),
+            ),
+            Positioned(
+                width: 350.0,
+                top: MediaQuery.of(context).size.height / 5,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                        width: 150.0,
+                        height: 150.0,
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    'https://pixel.nymag.com/imgs/daily/vulture/2017/06/14/14-tom-cruise.w700.h700.jpg'),
+                                fit: BoxFit.cover),
+                            borderRadius: BorderRadius.all(Radius.circular(75.0)),
+                            boxShadow: [
+                              BoxShadow(blurRadius: 7.0, color: Colors.black)
+                            ])),
+                    SizedBox(height: 90.0),
+                    Text(
+                      'Tom Cruise',
+                      style: TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Montserrat'),
+                    ),
+                    SizedBox(height: 15.0),
+                    Text(
+                      'Subscribe guys',
+                      style: TextStyle(
+                          fontSize: 17.0,
+                          fontStyle: FontStyle.italic,
+                          fontFamily: 'Montserrat'),
+                    ),
+                    SizedBox(height: 25.0),
+//                    Container(
+//                        height: 30.0,
+//                        width: 95.0,
+//                        child: Material(
+//                          borderRadius: BorderRadius.circular(20.0),
+//                          shadowColor: Colors.greenAccent,
+//                          color: Colors.green,
+//                          elevation: 7.0,
+//                          child: GestureDetector(
+//                            onTap: () {},
+//                            child: Center(
+//                              child: Text(
+//                                'Edit Name',
+//                                style: TextStyle(color: Colors.white, fontFamily: 'Montserrat'),
+//                              ),
+//                            ),
+//                          ),
+//                        )),
+                    SizedBox(height: 25.0),
+//                    Container(
+//                        height: 30.0,
+//                        width: 95.0,
+//                        child: Material(
+//                          borderRadius: BorderRadius.circular(20.0),
+//                          shadowColor: Colors.redAccent,
+//                          color: Colors.red,
+//                          elevation: 7.0,
+//                          child: GestureDetector(
+//                            onTap: () {},
+//                            child: Center(
+//                              child: Text(
+//                                'Log out',
+//                                style: TextStyle(color: Colors.white, fontFamily: 'Montserrat'),
+//                              ),
+//                            ),
+//                          ),
+//                        ))
+                  ],
+                ))
+          ],
+        ));
+  }
+}
+
+class getClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = new Path();
+
+    path.lineTo(0.0, size.height / 1.9);
+    path.lineTo(size.width + 125, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    return true;
+  }
+}
+
+
+
 
 //Main page widget state (displays the content of each tab)
 class _MyStatefulWidgetState extends State<MyStatefulWidget>
@@ -84,10 +198,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget>
   static List<Widget> _widgetOptions = <Widget>[
     displayHomeContent(), //widget function call
     displayMentorList(), // widget function call
-    Text(
-      'Your Personal Profile',
-      style: optionStyle,
-    ),
+    PersonalProfile(),
     Text(
       'Search for Other Potential Mentors here',
       style: optionStyle,
