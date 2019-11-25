2c33d219b0d221a8b21d82a1ec5ab36fdcab8b0b
David Scheuermann <dsmann1212@gmail.com>
2019-11-24 23:41:16 -0600

    Removed unnecessary import statements

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:impact/pages/root.dart';
+import 'package:impact/pages/login.dart';
 
 // main entry point
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  static const String _title = 'FLutter Demo';
+  static const String _title = 'Flutter Demo';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
-      home: RootPage(),
+      home: LoginScreen(),
     );
   }
 }
