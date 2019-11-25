03dade45e86e20f75676f772d0afc45205f84cec
David Scheuermann <dsmann1212@gmail.com>
2019-11-24 23:42:26 -0600

    Created service to handle user authentication, including sign in, sign up, logout, etc

--- lib/services/authentication.dart
+++ lib/services/authentication.dart
@@ -7,52 +7,47 @@ enum AuthStatus {
   LOGGED_IN,
 }
 
-abstract class BaseAuth {
-  Future<String> signIn(String email, String password);
+class AuthService {
+  static final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  Future<String> signUp(String email, String password);
-
-  Future<FirebaseUser> getCurrentUser();
-
-  Future<void> sendEmailVerification();
-
-  Future<void> signOut();
-
-  Future<bool> isEmailVerified();
-}
-
-class Auth implements BaseAuth {
-  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-
-  Future<String> signIn(String email, String password) async {
-    AuthResult result = await _firebaseAuth.signInWithEmailAndPassword(
-        email: email, password: password);
-    FirebaseUser user = result.user;
-    return user.uid;
+  static Future<String> signIn(String email, String password) async {
+    try {
+      AuthResult result = await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      return user.uid == null ? "User not found" : null;
+    } catch (exception) {
+      return exception.toString();
+    }
   }
 
-  Future<String> signUp(String email, String password) async {
-    AuthResult result = await _firebaseAuth.createUserWithEmailAndPassword(
-        email: email, password: password);
-    FirebaseUser user = result.user;
-    return user.uid;
+  static Future<String> signUp(String email, String password) async {
+    try {
+      AuthResult result = await _firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      // return user.uid;
+      return user.uid == null ? "Error occurred" : null;
+    } catch (exception) {
+      return exception.toString();
+    }
   }
 
-  Future<FirebaseUser> getCurrentUser() async {
+  static Future<FirebaseUser> getCurrentUser() async {
     FirebaseUser user = await _firebaseAuth.currentUser();
     return user;
   }
 
-  Future<void> signOut() async {
+  static Future<void> signOut() async {
     return _firebaseAuth.signOut();
   }
 
-  Future<void> sendEmailVerification() async {
+  static Future<void> sendEmailVerification() async {
     FirebaseUser user = await _firebaseAuth.currentUser();
     user.sendEmailVerification();
   }
 
-  Future<bool> isEmailVerified() async {
+  static Future<bool> isEmailVerified() async {
     FirebaseUser user = await _firebaseAuth.currentUser();
     return user.isEmailVerified;
   }
