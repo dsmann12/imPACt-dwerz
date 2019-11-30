6d9c5dfbcd2b57f81189f574d4cc4b35b5564a2e
Zheng Ye <zye3@lsu.edu>
2019-11-30 13:29:59 -0600

    Update profile.dart

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -5,6 +5,7 @@ import 'package:impact/pages/routes.dart';
 import 'package:impact/models/user.dart';
 
 
+
 class PersonalProfile extends StatefulWidget {
   @override
   _PersonalProfileState createState() => new _PersonalProfileState();
@@ -16,8 +17,8 @@ class _PersonalProfileState extends State<PersonalProfile> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        body: new Stack(
+    return Scaffold(
+        body: Stack(
           children: <Widget>[
             ClipPath(
               child: Container(color: Colors.black.withOpacity(1)),
@@ -32,129 +33,186 @@ class _PersonalProfileState extends State<PersonalProfile> {
                         width: 150.0,
                         height: 150.0,
                         decoration: BoxDecoration(
-                            color: Colors.red,
+//                            color: Colors.red,
                             image: DecorationImage(
-                                image: AssetImage(
-                                    'assets/profile_placeholder.png'),
+                                image: NetworkImage(
+                                    user.avatarURL),
                                 fit: BoxFit.cover),
                             borderRadius: BorderRadius.all(Radius.circular(75.0)),
                             boxShadow: [
                               BoxShadow(blurRadius: 7.0, color: Colors.black)
                             ])),
-                    SizedBox(height: 50.0),
-                    Column(
-                      children: <Widget>[
-                        Text(
-                          user.firstName + " " + user.lastName,
-                          style: TextStyle(
-                              fontSize: 30.0,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: 'Montserrat'),
-                        ),
-                        //SizedBox(height: 15.0),
-                        Chip(backgroundColor: Colors.blue, label: Text("Mentee"),),
-
-                        Text(
-                          user.institution,
-                          style: TextStyle(
-                              fontSize: 15.0,
-//                          fontStyle: FontStyle.italic,
-                              fontFamily: 'Montserrat'),
-                        ),
-                        //SizedBox(height: 5.0),
-
-                        Text(
-                          user.major,
-                          style: TextStyle(
-                              fontSize: 15.0,
-//                          fontStyle: FontStyle.italic,
-                              fontFamily: 'Montserrat'),
-                        ),
-
-                        Text(
-                          user.college,
-                          style: TextStyle(
-                              fontSize: 15.0,
-//                          fontStyle: FontStyle.italic,
-                              fontFamily: 'Montserrat'),
-                        ),
-
-                        Text(
-                          user.department.toString(),
-                          style: TextStyle(
-                              fontSize: 15.0,
-//                          fontStyle: FontStyle.italic,
-                              fontFamily: 'Montserrat'),
-                        ),
-
-//                        Text(
-//                          'Research Interest(s): '
-//                              'Software Development, '
-//                              ' Mobile Application Development, '
-//                              ' Web Application Development, '
-//                              ' Mobile and Web Security ',
-//                          style: TextStyle(
-//                              fontSize: 15.0,
+                    //SizedBox(height: 50.0),
+//                    Container(
+//                      child: Column(
+//                        //controller: scrollController,
+//                        children: <Widget>[
+//                          Text(
+//                            user.firstName + " " + user.lastName,
+//                            style: TextStyle(
+//                                fontSize: 30.0,
+//                                fontWeight: FontWeight.bold,
+//                                fontFamily: 'Montserrat'),
+//                          ),
+//                          //SizedBox(height: 15.0),
+//                          Chip(backgroundColor: Colors.blue, label: Text("Mentee"),),
+//                          Text(
+//                            user.description,
+//                            style: TextStyle(
+//                                fontSize: 15.0,
 ////                          fontStyle: FontStyle.italic,
-//                              fontFamily: 'Montserrat'),
-//                        ),
-                      ],
+//                                fontFamily: 'Montserrat'),
+//                          ),
+//
+//                          Text(
+//                            user.institution,
+//                            style: TextStyle(
+//                                fontSize: 15.0,
+////                          fontStyle: FontStyle.italic,
+//                                fontFamily: 'Montserrat'),
+//                          ),
+//                          //SizedBox(height: 5.0),
+//
+//                          Text(
+//                            user.major,
+//                            style: TextStyle(
+//                                fontSize: 15.0,
+////                          fontStyle: FontStyle.italic,
+//                                fontFamily: 'Montserrat'),
+//                          ),
+//
+//                          Text(
+//                            user.college,
+//                            style: TextStyle(
+//                                fontSize: 15.0,
+////                          fontStyle: FontStyle.italic,
+//                                fontFamily: 'Montserrat'),
+//                          ),
+//
+//                          Text(
+//                            user.department,
+//                            style: TextStyle(
+//                                fontSize: 15.0,
+////                          fontStyle: FontStyle.italic,
+//                                fontFamily: 'Montserrat'),
+//                          ),
+//
+//                          Text(
+//                            'Research Interest(s): '
+//                                'Software Development, '
+//                                ' Mobile Application Development, '
+//                                ' Web Application Development, '
+//                                ' Mobile and Web Security ',
+//                            style: TextStyle(
+//                                fontSize: 15.0,
+////                          fontStyle: FontStyle.italic,
+//                                fontFamily: 'Montserrat'),
+//                          ),
+//
+//
+//                          Row(
+//                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                            children: <Widget>[
+//                              IconButton(
+//                                icon: Icon(Icons.edit),
+//                                tooltip: 'Tap to edit profile',
+//                                onPressed: () {
+//
+//                                },
+//                              ),
+//                              IconButton(
+//                                icon: Icon(Icons.power_settings_new),
+//                                tooltip: 'Tap to logout',
+//                                onPressed: () {
+//                                  AuthService.signOut();
+//                                  Navigator.of(context).pushReplacement(FadePageRoute(
+//                                      builder: (context) => LoginScreen()
+//                                  ));
+//                                },
+//                              ),
+//                            ],
+//                          ),
+//
+//                  ],
+//                )),
+          ],
+
+        ),
+    ),
+            Container(
+                child: ListView(
+                  shrinkWrap: true,
+                  padding: EdgeInsets.fromLTRB(5.0, 300.0, 10.0, 0.0),
+                  children: <Widget>[
+                    Text(
+                      user.firstName + " " + user.lastName,
+                      style: TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Montserrat'),
+                      textAlign: TextAlign.center,
                     ),
-//                    Text(
-//                      'Zheng Ye',
-//                      style: TextStyle(
-//                          fontSize: 30.0,
-//                          fontWeight: FontWeight.bold,
-//                          fontFamily: 'Montserrat'),
-//                    ),
-//                    //SizedBox(height: 15.0),
-//                    Chip(backgroundColor: Colors.blue, label: Text("Mentee"),),
-//
-//                    Text(
-//                      'Institution: Lousiana State University',
-//                      style: TextStyle(
-//                          fontSize: 15.0,
-////                          fontStyle: FontStyle.italic,
-//                          fontFamily: 'Montserrat'),
-//                    ),
-//                    //SizedBox(height: 5.0),
-//
-//                    Text(
-//                      'Major: Computer Science',
-//                      style: TextStyle(
-//                          fontSize: 15.0,
-////                          fontStyle: FontStyle.italic,
-//                          fontFamily: 'Montserrat'),
-//                    ),
-//
-//                    Text(
-//                      'College: College of Engineering',
-//                      style: TextStyle(
-//                          fontSize: 15.0,
-////                          fontStyle: FontStyle.italic,
-//                          fontFamily: 'Montserrat'),
-//                    ),
-//
-//                    Text(
-//                      'Department: Computer Science',
-//                      style: TextStyle(
-//                          fontSize: 15.0,
-////                          fontStyle: FontStyle.italic,
-//                          fontFamily: 'Montserrat'),
-//                    ),
-//
-//                    Text(
-//                      'Research Interest(s): '
-//                          'Software Development, '
-//                          ' Mobile Application Development, '
-//                          ' Web Application Development, '
-//                          ' Mobile and Web Security ',
-//                      style: TextStyle(
-//                          fontSize: 15.0,
-////                          fontStyle: FontStyle.italic,
-//                          fontFamily: 'Montserrat'),
-//                    ),
-                    //SizedBox(height: 2.0),
+                    //SizedBox(height: 15.0),
+                    Chip(backgroundColor: Colors.deepPurple, label: Text("Mentee"), ),
+                    Text(
+                      user.description,
+                      style: TextStyle(
+                          fontSize: 15.0,
+//                          fontStyle: FontStyle.italic,
+                          fontFamily: 'Montserrat'),
+                    ),
+
+                    Text(
+                      user.institution,
+                      style: TextStyle(
+                          fontSize: 15.0,
+//                          fontStyle: FontStyle.italic,
+                          fontFamily: 'Montserrat'),
+                    ),
+                    //SizedBox(height: 5.0),
+
+                    Text(
+                      user.major,
+                      style: TextStyle(
+                          fontSize: 15.0,
+//                          fontStyle: FontStyle.italic,
+                          fontFamily: 'Montserrat'),
+                    ),
+
+                    Text(
+                      user.college,
+                      style: TextStyle(
+                          fontSize: 15.0,
+//                          fontStyle: FontStyle.italic,
+                          fontFamily: 'Montserrat'),
+                    ),
+
+                    Text(
+                      user.department,
+                      style: TextStyle(
+                          fontSize: 15.0,
+//                          fontStyle: FontStyle.italic,
+                          fontFamily: 'Montserrat'),
+                    ),
+
+                    Text(
+                      'Research Interest(s): '
+                          'Software Development, '
+                          ' Mobile Application Development, '
+                          ' Web Application Development, '
+                          ' Mobile and Web Security ',
+                      style: TextStyle(
+                          fontSize: 15.0,
+//                          fontStyle: FontStyle.italic,
+                          fontFamily: 'Montserrat'),
+                    ),
+
+                    Text(
+                      'Mentors: ' +
+                      user.mentors[0] + ', ' + user.mentors[1] + ', ' + user.mentors[2] + ', '  + user.mentors[3],
+                    ),
+
+
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: <Widget>[
@@ -177,49 +235,16 @@ class _PersonalProfileState extends State<PersonalProfile> {
                         ),
                       ],
                     ),
-//                    Container(
-//                        height: 30.0,
-//                        width: 95.0,
-//                        child: Material(
-//                          borderRadius: BorderRadius.circular(20.0),
-//                          shadowColor: Colors.greenAccent,
-//                          color: Colors.green,
-//                          elevation: 7.0,
-//                          child: GestureDetector(
-//                            onTap: () {},
-//                            child: Center(
-//                              child: Text(
-//                                'Edit Name',
-//                                style: TextStyle(color: Colors.white, fontFamily: 'Montserrat'),
-//                              ),
-//                            ),
-//                          ),
-//                        )),
-                    SizedBox(height: 25.0),
-//                    Container(
-//                        height: 30.0,
-//                        width: 95.0,
-//                        child: Material(
-//                          borderRadius: BorderRadius.circular(20.0),
-//                          shadowColor: Colors.redAccent,
-//                          color: Colors.red,
-//                          elevation: 7.0,
-//                          child: GestureDetector(
-//                            onTap: () {},
-//                            child: Center(
-//                              child: Text(
-//                                'Log out',
-//                                style: TextStyle(color: Colors.white, fontFamily: 'Montserrat'),
-//                              ),
-//                            ),
-//                          ),
-//                        ))
+
                   ],
-                ))
-          ],
-        ));
+                )),
+    ],
+    ),
+    );
   }
 }
+
+
 
 class getClipper extends CustomClipper<Path> {
   @override
